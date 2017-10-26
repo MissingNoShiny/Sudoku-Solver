5b6fa2f5ef83988b7763a6fde67f13cacf619117
Vincent Larcin <vincent.larcin@student.umons.ac.be>
2017-10-26 22:46:24 +0200

    Command lines arguments
    + solve() now returns the solution instead of printing it
    + Added console arguments to choose an input file and an optional output
    file
    - Removed the objective function since it was useless

--- Solver.py
+++ Solver.py
@@ -4,6 +4,7 @@ Created on 25 oct. 2017
 @author: Vincent Larcin
 '''
 
+import argparse
 from pulp import *
 
 def create_starting_numbers(path):
@@ -26,8 +27,9 @@ def create_starting_numbers(path):
 
 def solve(starting_numbers):
     """
-    Solves a sudoku problem given its starting numbers.
+    Solves a sudoku problem given its starting numbers and returns found solution
     @param starting_numbers: The starting numbers of the sudoku problem
+    @return A two-dimensional list that contains the solution, or None if there is no solution
     """
     
     sudoku_problem = LpProblem("Sudoku", LpMinimize)
@@ -47,8 +49,8 @@ def solve(starting_numbers):
     #Creates the 729 variables
     variables = LpVariable.dict("Variables", (values, values, values), 0, 1, LpInteger)
     
-    #Objective function
-    sudoku_problem += lpSum(variables[(column, row, possibility)] for column in values for row in values for possibility in values), "Objective function"
+    #No objective function is needed since a solution matching the constraints is always optimal
+    sudoku_problem += 1, "No objective function"
     
     #Constraints to have only one possibility per cell
     for column in values:
@@ -65,7 +67,7 @@ def solve(starting_numbers):
         for column in values:
             sudoku_problem += lpSum([variables[(column, row, possibility)] for row in values]) == 1, ""
             
-        #Constraint to have only one of each possibility in each box
+        #Constraints to have only one of each possibility in each box
         for box in boxes:
             sudoku_problem += lpSum([variables[(column, row, possibility)] for (row, column) in box]) == 1, ""
     
@@ -77,26 +79,52 @@ def solve(starting_numbers):
     sudoku_problem.writeLP("Sudoku.lp")
     sudoku_problem.solve()
     
-    if sudoku_problem.status == 1:
-        print("Solution found!\n")
-    else:
-        print("No solution found.")
+    if sudoku_problem.status != 1:
+        return None
     
-    #Displays the solution
+    solution = []
+    
     for row in values:
-        line = ""
+        line = []
         for column in values:
             for possibility in values:
                 if value(variables[(column, row, possibility)]) == 1:
-                    line += str(possibility)
-            if column in (3, 6):
-                line += "|"
-        print(line)
-        if row in (3, 6):
-            print("---+---+---")
-            
+                    line.append(possibility)
+        solution.append(line)
     
+    return solution
     
 if __name__ == '__main__':
-    starting_numbers = create_starting_numbers("example0.sdk")
-    solve(starting_numbers)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="path to the input file")
+    parser.add_argument("-o", "--output", metavar="output", help="path to the output file")
+    args = parser.parse_args()
+    
+    starting_numbers = create_starting_numbers(args.input)
+    solution = solve(starting_numbers)
+    
+    if solution == None:
+        print("No solution found.")
+    
+    else:
+        
+        print("Solution found!\n")
+        
+        #Writes the solution in the chosen output file
+        if args.output != None:
+            with open(args.output, "w") as file:
+                for line in solution:
+                    file.writelines(str(el) for el in line)
+                    file.write("\n")
+    
+        #Displays the solution
+        for i, row in enumerate(solution):
+            line = ""
+            for j, el in enumerate(row):
+                line += str(el)
+                if j in (2, 5):
+                    line += "|"
+            print(line)
+            if i in (2, 5):
+                print("---+---+---")
